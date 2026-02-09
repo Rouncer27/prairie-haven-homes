f38b55a54e7db2a2901746c19802877f8fffed03
Trevor Rounce <trevor@switchbackcreative.ca>
2026-02-09 10:31:03 -0700

    project setup

---
+++ src/data/siteData.js
@@ -0,0 +1,11 @@
+import defaultSrc from "../../public/meta-default-image.jpg";
+
+export const baseData = {
+  title: "Prairie Haven Homes",
+  organizationName: "Prairie Haven Home",
+  description: "Prairie Haven Home",
+  siteImg: `https://prairie-haven-homes.netlify.app/${defaultSrc.src}`,
+  logoUrl: "",
+  url: "https://prairie-haven-homes.netlify.app/",
+  socialProfiles: [],
+};
