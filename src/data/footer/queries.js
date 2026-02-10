0e627cdf0a81faa0cb298d403e1564c2b293ceff
Trevor Rounce <trevor@switchbackcreative.ca>
2026-02-10 08:57:30 -0700

    navigation work

---
+++ src/data/footer/queries.js
@@ -0,0 +1,54 @@
+export const Logo_Options_Query = `
+     footerLogo: siteWideSettings {
+        logoOptions {
+            footerLogo {
+                node {
+                    altText
+                    sourceUrl
+                     mediaDetails {
+                        height
+                        width
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const Footer_Menu_Query = `
+     footerMenu: menu(id: "Footer Menu", idType: NAME) {
+        menuItems(first: 99999) {
+            nodes {
+                label
+                uri
+                parentId
+                cssClasses
+                childItems {
+                    nodes {
+                        label
+                        uri
+                    }
+                }
+            }
+        }
+    }
+`;
+
+export const Social_Media_Links_Query = `
+     socialMediaLinks: siteWideSettings {
+        socialMedia {
+            facebookLink
+            instagramLink
+            linkedinLink
+            youtubeLink
+        }
+    }
+`;
+
+export const Contact_Information_Query = `
+     contactInformation: siteWideSettings {
+        contactOptions {
+            phoneNumber
+        }
+    }
+`;
