8daea307ed91005786435a91a01552f3e8cfb457
Trevor Rounce <trevor@switchbackcreative.ca>
2026-02-10 13:40:53 -0700

    work on the home page

--- src/data/home/queries.js
+++ src/data/home/queries.js
@@ -48,3 +48,102 @@ export const Page_Intro_Query = `
         }
     }
 `;
+
+export const Key_Features_Query = `
+   	 keyFeatures: keyFeaturesComponent {
+        title
+        features {
+            feature
+        }
+    }
+`;
+
+export const Content_Boxes_Query = `
+   	 contentBoxes: contentBoxesComponent {
+        boxOneImage {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        boxOneBackground {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        boxOneContent
+        boxOneButtonText
+        boxOneButtonLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+        boxTwoImage {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        boxTwoBackground {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        boxTwoContent
+    }
+`;
+
+export const Steps_Query = `
+   	 steps: stepsComponent {
+        title
+        subTitle
+        backgroundImage {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        stepsImage {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        buttonText
+        buttonLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+    }
+`;
