47f402367ed0c481516ef656450ddc0d4d854f29
Trevor Rounce <trevor@switchbackcreative.ca>
2026-02-10 09:47:45 -0700

    navigation work

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,11 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    css: {
+      devSourcemap: true, // ✅ Enables CSS/SCSS source maps for better debugging
+    },
+  },
   integrations: [react()],
   image: {
     domains: ["prairiehaven.swbdatabases.ca/"],
