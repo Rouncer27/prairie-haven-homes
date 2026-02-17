36b93d72cdafd2a7857187c8d6e8244c6c3c4ec4
Trevor Rounce <trevor@switchbackcreative.ca>
2026-02-17 12:19:09 -0700

    work on the haven homes page

--- src/data/havenHomes/queries.js
+++ src/data/havenHomes/queries.js
@@ -4,3 +4,73 @@ export const Side_Side_Intro_Query = `
         content
     }
 `;
+
+export const Floorplans_Query = `
+   	floorplans: floorplansComponent {
+        displayFloorplans
+        title
+    }
+`;
+
+export const Content_Boxes_Query = `
+   	 contentBoxes: contentBoxesComponent {
+        boxOneImage {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        boxOneBackground {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        boxOneContent
+        boxOneButtonText
+        boxOneButtonLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+        boxTwoImage {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        boxTwoBackground {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
+        }
+        boxTwoContent
+        boxTwoButtonText
+        boxTwoButtonLink {
+            edges {
+                node {
+                    uri
+                }
+            }
+        }
+    }
+`;
