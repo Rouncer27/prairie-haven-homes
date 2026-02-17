b81bd57e07a1572d92435e78e689f5350657038c
Trevor Rounce <trevor@switchbackcreative.ca>
2026-02-17 16:37:01 -0700

    floorplans post

--- src/data/floorPlans/queries.js
+++ src/data/floorPlans/queries.js
@@ -1,6 +1,22 @@
 export const Hero_Titles_Query = `
-   	    title
-        floorplanPost {
-            specs
+    heroTitles: heroTitlesComponent {
+        title
+    }
+`;
+
+export const Floorplan_Intro_Query = `
+    floorplanIntro: floorplanIntroComponent {
+        image {
+            node {
+                sourceUrl
+                altText
+                mediaDetails {
+                    width
+                    height
+                }
+            }
         }
+        title
+        content
+    }
 `;
